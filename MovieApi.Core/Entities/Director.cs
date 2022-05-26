8c9f13b7ec23e72565843f2a96b550c26a7e0793
akcy01 <98681852+akcy01@users.noreply.github.com>
2022-05-26 22:16:03 +0300

    Add project files.

---
+++ MovieApi.Core/Entities/Director.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApi.Core.Entities
+{
+    public class Director
+    {
+
+        public int Id { get; set; }
+        public string DirectorName { get; set; }
+        public ICollection<MovieModel> movies { get; set; } //1'e çok ilişki.
+
+    }
+}
